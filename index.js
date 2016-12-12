7ece0d186a4393191ce9c80436b5fefbc1d6829a
otherUser <2wnaud@naver.com>
2016-12-12 14:59:49 +0900

    스크립트 변경

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var btn = document.getElementById("login");
 var reg = document.getElementById("regist");
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(){
     alert("로그인버튼을 클릭했습니다.");
+    var userId = document.getElementById("userId").value;
+    var userPass = document.getElementById("userPass").value;
+    console.log(userId);
+    if(userId != "" && userPass != ""){
+        alert("로그인에 성공했습니다.");
+    }else{
+        alert("로그인에 실패했습니다.");
+    }
 });
-reg.addEventListener('click', function() {
+reg.addEventListener('click', function(){
     alert("회원가입버튼을 클릭했습니다.");
 });
